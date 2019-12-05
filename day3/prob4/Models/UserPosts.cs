6f9043428feb08bd73c3ca3e27a0e3b027b80727
florin.ghinita <florin.ghinita@traveltech.dotw.com>
2019-12-05 13:36:20 +0200

    prob4

---
+++ day3/prob4/Models/UserPosts.cs
@@ -0,0 +1,11 @@
+﻿namespace LinqAndLamdaExpressions.Models
+{
+    using System.Collections.Generic;
+
+    public class UserPosts
+    {
+        public User User { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
